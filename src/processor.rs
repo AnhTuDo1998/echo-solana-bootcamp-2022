46ee4ab2c0f4b045608010091235f0e0d2607b09
Do Anh Tu <anhtu.do1998@gmail.com>
2022-03-29 22:11:03 +0800

    Scaffold with bootcamp lecture1 code

---
+++ src/processor.rs
@@ -0,0 +1,22 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+use crate::instruction::Instruction;
+
+pub struct Processor {}
+
+impl Processor {
+    pub fn process_instruction(
+        _program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        Ok(())
+    }
+}
