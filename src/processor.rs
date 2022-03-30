e8536f40784d14d74b1def45b46c63625830c8fc
Do Anh Tu <anhtu.do1998@gmail.com>
2022-03-30 08:00:24 +0000

    Added skeleton unit test for echo

--- src/processor.rs
+++ src/processor.rs
@@ -28,3 +28,33 @@ impl Processor {
         Ok(())
     }
 }
+
+// Unit test
+#[cfg(test)]
+mod test {
+    use super::*;
+    use solana_program::instruction::{AccountMeta, Instruction};
+    use solana_program_test::*;
+    use solana_sdk::{signature::Signer, transaction::Transaction};
+
+    #[tokio::test]
+    async fn test_echo() {
+        let program_id = Pubkey::new_unique();
+
+        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
+            "echo_solana_bootcamp",
+            program_id,
+            processor!(Processor::process_instruction)).start().await;
+        
+        // Transaction (signed) that contains the Ix Echo payload
+        let mut transaction = Transaction::new_with_payer(
+            &[Instruction::new_with_borsh(
+                program_id,
+                &vec![1, 2, 3],
+                vec![AccountMeta::new(payer.pubkey(), false)], //writable account
+            )],
+            Some(&payer.pubkey()),
+        );
+        transaction.sign(&[&payer], recent_blockhash);
+    }
+}
