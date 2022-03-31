9c4599856ccaab167fc88996dffe507365b37803
Do Anh Tu <anhtu.do1998@gmail.com>
2022-03-31 23:26:57 +0800

    First implement EchoInstruction::Echo

--- src/processor.rs
+++ src/processor.rs
@@ -17,15 +17,49 @@ impl Processor {
         accounts: &[AccountInfo],
         instruction_data: &[u8],
     ) -> ProgramResult {
-
         let instruction = EchoInstruction::try_from_slice(instruction_data)
             .map_err(|_| ProgramError::InvalidInstructionData)?;
 
         match instruction {
-            EchoInstruction::Echo {data} => {
+            EchoInstruction::Echo { data } => {
                 msg!("Echo Instruction");
-                
-            },
+                let account_info_iter = &mut accounts.iter();
+                let echo_account_info = next_account_info(account_info_iter)?;
+                msg!(
+                    "Echo Buffer Account: signer: {}, writeable: {}",
+                    echo_account_info.is_signer,
+                    echo_account_info.is_writable
+                );
+
+                let mut echo_buffer = echo_account_info.data.borrow_mut();
+                msg!("Initial Echo Buffer from Account: {:?}", echo_buffer);
+
+                // Check if echo_buffer is non-zero
+                let has_non_zero: bool = echo_buffer.into_iter().any(|byte| *byte != 0);
+                if has_non_zero {
+                    // Fail immediately
+                    msg!("Echo Buffer has non-zero data. Quitting...");
+                    // TODO: Maybe some more meaningful Error...
+                    return Err(ProgramError::InvalidInstructionData);
+                }
+
+                // Check the echo_buffer is not allocated
+                if echo_buffer.len() == 0 {
+                    // Fail immediately
+                    msg!("Echo Buffer is not allocated. Quitting...");
+                    // TODO: Maybe some more meaningful Error...
+                    return Err(ProgramError::InvalidInstructionData);
+                }
+
+                msg!("Copy from EchoInstruction{data} to on-chain echo_buffer");
+                // Copy N bytes to echo_buffer where N is its length
+                for it in data.iter().zip(echo_buffer.into_iter()) {
+                    let (from, to) = it;
+                    *to = *from;
+                }
+                msg!("Copied Echo Buffer from Account: {:?}", echo_buffer);
+                //echo_buffer.serialize(&mut *echo_account_info.data.borrow_mut()).map_err(|_| ProgramError::InvalidInstructionData)?
+            }
             _ => unimplemented!(),
         }
 
--- tests/functional.rs
+++ tests/functional.rs
@@ -3,20 +3,23 @@ use solana_program::pubkey::Pubkey;
 use solana_program_test::*;
 use solana_sdk::{signature::Signer, transaction::Transaction};
 
-use echo_solana_bootcamp::{
-    processor::Processor,
-    instruction::echo,
-};
+use echo_solana_bootcamp::{instruction::echo, processor::Processor};
 
 #[tokio::test]
 async fn test_echo_program() {
-    let program_id = Pubkey::new_unique();
+    // TODO: Create user keypair & allocate on-chain data buffer...
 
+    // PK for "on-chain" program
+    let program_id = Pubkey::new_unique();
     let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
         "echo_solana_bootcamp",
         program_id,
-        processor!(Processor::process)).start().await;
-    
+        processor!(Processor::process),
+    )
+    .start()
+    .await;
+
+    // Instruction arg/parameter
     let echo_data: Vec<u8> = vec![1, 2, 3];
 
     // Transaction (signed) that contains the Ix Echo payload
@@ -27,4 +30,12 @@ async fn test_echo_program() {
     transaction.sign(&[&payer], recent_blockhash);
 
     banks_client.process_transaction(transaction).await.unwrap();
+
+    let echo_account_data_after = banks_client
+        .get_account(payer.pubkey())
+        .await
+        .unwrap()
+        .unwrap();
+    let echo_buffer_data_after = echo_account_data_after.data.as_slice();
+    assert_eq!(echo_buffer_data_after, vec![1, 2, 3])
 }
