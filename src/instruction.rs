c7034e42bcc3d8a278ef3df365dacd272e2f6aa5
Do Anh Tu <anhtu.do1998@gmail.com>
2022-03-31 21:33:04 +0800

    Add utility function to build EchoInstruction::Echo

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,4 +1,10 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
 pub enum EchoInstruction {
     // Required
@@ -24,4 +30,27 @@ pub enum EchoInstruction {
     VendingMachineEcho {
         data: Vec<u8>,
     },
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn echo(
+    program_id: &Pubkey,
+    echo_key: &Pubkey,
+    echo_data: Vec<u8>,
+) -> Result<Instruction, ProgramError> {
+    // Pack an Echo instruction using Borsh
+    let init_data = EchoInstruction::Echo{data: echo_data};
+    let data = init_data.try_to_vec()?;
+
+    // Pack account for Echo
+    let accounts = vec![
+        AccountMeta::new(*echo_key,false)
+    ];
+    
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+
 }
