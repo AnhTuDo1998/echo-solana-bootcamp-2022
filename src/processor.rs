7c177faddc4efbce39571b5f25342ae087c8cb8e
Do Anh Tu <anhtu.do1998@gmail.com>
2022-03-31 21:32:27 +0800

    Remove unit test & rename process for readibility

--- src/processor.rs
+++ src/processor.rs
@@ -12,53 +12,23 @@ use crate::instruction::EchoInstruction;
 pub struct Processor {}
 
 impl Processor {
-    pub fn process_instruction(
+    pub fn process(
         _program_id: &Pubkey,
         accounts: &[AccountInfo],
         instruction_data: &[u8],
     ) -> ProgramResult {
+
         let instruction = EchoInstruction::try_from_slice(instruction_data)
             .map_err(|_| ProgramError::InvalidInstructionData)?;
 
         match instruction {
-            EchoInstruction::Echo {data} => unimplemented!(),
+            EchoInstruction::Echo {data} => {
+                msg!("Echo Instruction");
+                
+            },
             _ => unimplemented!(),
         }
 
         Ok(())
-    }
-}
-
-// Unit test
-#[cfg(test)]
-mod test {
-    use super::*;
-    use solana_program::instruction::{AccountMeta, Instruction};
-    use solana_program_test::*;
-    use solana_sdk::{signature::Signer, transaction::Transaction};
-
-    #[tokio::test]
-    async fn test_echo() {
-        // TODO: This is more of integration test.
-        // Simplify unit testing, or remove it completely
-        let program_id = Pubkey::new_unique();
-
-        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
-            "echo_solana_bootcamp",
-            program_id,
-            processor!(Processor::process_instruction)).start().await;
-        
-        // Transaction (signed) that contains the Ix Echo payload
-        let mut transaction = Transaction::new_with_payer(
-            &[Instruction::new_with_borsh(
-                program_id,
-                &vec![1u8],
-                vec![AccountMeta::new(payer.pubkey(), false)], //writable account
-            )],
-            Some(&payer.pubkey()),
-        );
-        transaction.sign(&[&payer], recent_blockhash);
-
-        banks_client.process_transaction(transaction).await.unwrap();
     }
 }
